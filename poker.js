e49b62f947a9adbf9cdfb3b3c3faa26733c21618
dd <remix@gmail.com>
2022-09-16 00:32:46 +0800

    前端作品-賭聖

--- poker.js
+++ poker.js
@@ -100,11 +100,13 @@ function analytics() {
         if (data1[0] > data1[1]) {
             final.innerText = ('您跑錯棚了?!')
             final.style.color = 'green';
+            let qqpic = ["qq.jfif"];
+            king.src = `${qqpic[0]}`
         }
         else {
             final.innerText = ('賭聖');
             final.style.color = 'blue';
-            let kingpic = ['./ king.jpg']
+            let kingpic = ["king.jpg"];
             king.src = `${kingpic[0]}`
         }
 
